0769bb84b7c160543d7ce9eb465b75e97aa764f1
Bohan <yang@bohan.co>
2022-12-15 14:27:44 +0000

    Update

--- vite.config.js
+++ vite.config.js
@@ -1,38 +1,41 @@
-import { defineConfig, loadEnv } from "vite"
-import symfonyPlugin from "vite-plugin-symfony"
+import { defineConfig, loadEnv } from 'vite'
+import symfonyPlugin from 'vite-plugin-symfony'
 import react from '@vitejs/plugin-react'
 import inject from '@rollup/plugin-inject'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), 'VITE_')
-    return {
-        plugins: [
-            inject({
-                jQuery: 'jquery',
-            }),
-            react(),
-            symfonyPlugin(),
-        ],
-        resolve: {
-            alias: {
-                "@/": `${__dirname}/assets/`,
-            },
-        },
-        build: {
-            rollupOptions: {
-                input: {
-                    app: "./assets/bootstrap.tsx",
-                    admin: "./assets/admin/bootstrap.tsx"
-                },
-            },
-        },
-        server: {
-            host: env.VITE_LISTEN_HOST,
-            port: env.VITE_LISTEN_PORT,
-            origin: env.VITE_ORIGIN,
-        },
-        optimizeDeps: {
-            include: ["jquery"],
-        },
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  return {
+    plugins: [
+      inject({
+        jQuery: 'jquery'
+      }),
+      react(),
+      symfonyPlugin(),
+      viteCompression({ algorithm: 'gzip' }),
+      viteCompression({ algorithm: 'brotliCompress' })
+    ],
+    resolve: {
+      alias: {
+        '@/': `${__dirname}/assets/`
+      }
+    },
+    build: {
+      rollupOptions: {
+        input: {
+          app: './assets/bootstrap.tsx',
+          admin: './assets/admin/bootstrap.tsx'
+        }
+      }
+    },
+    server: {
+      host: env.VITE_LISTEN_HOST,
+      port: env.VITE_LISTEN_PORT,
+      origin: env.VITE_ORIGIN
+    },
+    optimizeDeps: {
+      include: ['jquery']
     }
+  }
 })
