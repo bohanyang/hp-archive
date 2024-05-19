7e73f1ba59dca7c0b0f2f2c9dabc6aafebe94e2c
Bohan Yang <8384161+bohanyang@users.noreply.github.com>
2024-05-19 09:11:14 +0000

    Fix preloading

--- src/Bundle/Doctrine/Middleware/ExceptionConverterMiddleware.php
+++ src/Bundle/Doctrine/Middleware/ExceptionConverterMiddleware.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Bundle\Doctrine\Middleware;
 
 use Doctrine\DBAL\Driver;
+use Doctrine\DBAL\Driver\API\ExceptionConverter as ExceptionConverterInterface;
 use Doctrine\DBAL\Driver\Middleware;
 use Doctrine\DBAL\Driver\Middleware\AbstractDriverMiddleware;
 
@@ -18,7 +19,7 @@ class ExceptionConverterMiddleware implements Middleware
     public function wrap(Driver $driver): Driver
     {
         return new class ($driver) extends AbstractDriverMiddleware {
-            public function getExceptionConverter(): ExceptionConverter
+            public function getExceptionConverter(): ExceptionConverterInterface
             {
                 return new ExceptionConverter(parent::getExceptionConverter());
             }
