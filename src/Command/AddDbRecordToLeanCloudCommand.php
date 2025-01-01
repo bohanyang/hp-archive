01ceed23e279d9fd288fbdc8f79d6ea244df4248
Brent, Yang Bohan <brent@rainbowedge.co>
2025-01-01 02:09:52 +0000

    add

---
+++ src/Command/AddDbRecordToLeanCloudCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Command;
+
+use App\Bundle\Repository\DoctrineRepository;
+use App\Bundle\Repository\LeanCloudRepository;
+use DateTimeImmutable;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+
+#[AsCommand(name: 'app:add-record')]
+class AddDbRecordToLeanCloudCommand extends Command
+{
+    public function __construct(
+        private MessageBusInterface $messageBus,
+        private LeanCloudRepository $leanCloud,
+        private DoctrineRepository $doctrine,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this->addArgument('market', InputArgument::REQUIRED, 'Record Market');
+        $this->addArgument('date', InputArgument::REQUIRED, 'Record Date');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $record = $this->doctrine->getRecord($input->getArgument('market'), new DateTimeImmutable($input->getArgument('date')));
+        $this->messageBus->dispatch($this->leanCloud->createRecordRequest($record));
+
+        return 0;
+    }
+}
