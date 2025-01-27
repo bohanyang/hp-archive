70d3f5f76fafb2510265c6c7c170dae3a56b648b
Brent, Yang Bohan <brent@rainbowedge.co>
2025-01-27 13:59:14 +0000

    FUCK

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,36 +1,34 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = 'php' ] || [ "$1" = 'bin/console' ] || [ "$1" = 'rr' ]; then
-	if grep -q ^DATABASE_URL= .env; then
-		echo "Waiting for database to be ready..."
-		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
-		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
-			if [ $? -eq 255 ]; then
-				# If the Doctrine command exits with 255, an unrecoverable error occurred
-				ATTEMPTS_LEFT_TO_REACH_DATABASE=0
-				break
-			fi
-			sleep 1
-			ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
-			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
-		done
-
-		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
-			echo "The database is not up or not reachable:"
-			echo "$DATABASE_ERROR"
-			exit 1
-		else
-			echo "The database is now ready and reachable"
+if grep -q ^DATABASE_URL= .env; then
+	echo "Waiting for database to be ready..."
+	ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+	until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console dbal:run-sql -q "SELECT 1" 2>&1); do
+		if [ $? -eq 255 ]; then
+			# If the Doctrine command exits with 255, an unrecoverable error occurred
+			ATTEMPTS_LEFT_TO_REACH_DATABASE=0
+			break
 		fi
+		sleep 1
+		ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+		echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+	done
 
-		if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
-			php bin/console doctrine:migrations:migrate --no-interaction
-		fi
+	if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+		echo "The database is not up or not reachable:"
+		echo "$DATABASE_ERROR"
+		exit 1
+	else
+		echo "The database is now ready and reachable"
 	fi
 
-	setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
-	setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
+	if [ "$( find ./migrations -iname '*.php' -print -quit )" ]; then
+		php bin/console doctrine:migrations:migrate --no-interaction
+	fi
 fi
+
+setfacl -R -m u:www-data:rwX -m u:"$(whoami)":rwX var
+setfacl -dR -m u:www-data:rwX -m u:"$(whoami)":rwX var
 
 exec docker-php-entrypoint "$@"
