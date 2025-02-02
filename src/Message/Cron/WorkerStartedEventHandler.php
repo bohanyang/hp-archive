d86be5b116f143b8475134be3f92a7f9cd4e868c
Brent, Yang Bohan <brent@rainbowedge.co>
2025-02-02 06:58:30 +0000

    fix

--- src/Message/Cron/WorkerStartedEventHandler.php
+++ src/Message/Cron/WorkerStartedEventHandler.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 namespace App\Message\Cron;
 
-use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Messenger\Event\WorkerStartedEvent;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\DispatchAfterCurrentBusStamp;
 
-#[AsMessageHandler]
+#[AsEventListener]
 class WorkerStartedEventHandler
 {
     public function __construct(private MessageBusInterface $bus)
