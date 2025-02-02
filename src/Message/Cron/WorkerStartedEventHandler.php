6c1771a9ae023e7a40c153640c4b17d65213703d
Brent, Yang Bohan <brent@rainbowedge.co>
2025-02-02 06:31:14 +0000

    Auto crawl once after messenger consumer started

---
+++ src/Message/Cron/WorkerStartedEventHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Message\Cron;
+
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\Event\WorkerStartedEvent;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\DispatchAfterCurrentBusStamp;
+
+#[AsMessageHandler]
+class WorkerStartedEventHandler
+{
+    public function __construct(private MessageBusInterface $bus)
+    {
+    }
+
+    public function __invoke(WorkerStartedEvent $event): void
+    {
+        $this->bus->dispatch(new HourlyCrawl(), [
+            new DispatchAfterCurrentBusStamp(),
+        ]);
+    }
+}
